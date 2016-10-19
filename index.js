fc9cb778ecd63ee336dc95fe7b2a5283864d2f74
Luke Edwards <luke.edwards05@gmail.com>
2016-10-19 22:27:37 +0200

    update default browsers array

--- index.js
+++ index.js
@@ -3,7 +3,17 @@
 const postcss = require('postcss');
 const prefixer = require('autoprefixer');
 
-const browsers = ['last 5 versions'];
+const browsers = [
+	'ie >= 10',
+	'ie_mob >= 10',
+	'ff >= 30',
+	'chrome >= 34',
+	'safari >= 7',
+	'opera >= 23',
+	'ios >= 7',
+	'android >= 4.4',
+	'bb >= 10'
+];
 
 module.exports = function () {
 	this.plugin('autoprefixer', {}, function * (file, opts) {
